e6e3214db30e70603ba62ff2aaaf1f2ea12bc7d8
yods <yodit_berga@yahoo.co.uk>
2013-10-21 00:20:37 +0100

    tidy up and correction hello world

--- src/mqtt_demo/core.clj
+++ src/mqtt_demo/core.clj
@@ -2,9 +2,7 @@
   (:gen-class)
   (:require [clojurewerkz.machine-head.client :as mh]))
 
-(def id (mh/generate-id))
-(def conn (mh/connect "tcp://127.0.0.1:1883" id))
-
+(def conn (mh/connect "tcp://127.0.0.1:1883" (mh/generate-id)))
 (defn subscribes []
   (mh/subscribe conn ["hello"] (fn [topic meta payload]
                                  (println (String. payload "UTF-8"))))
@@ -12,27 +10,19 @@
 
 (subscribes)
 
-(defn over-pollution)
+(defn over-pollution [limit]
+  (mh/subscribe conn ["Pollution"]
+                (fn [topic meta payload]
+                  (let [pollution payload]
+                    (if (> pollution limit)
+                      (println (format "Danger: Pollution is over %s limit and is at %s level" limit pollution)))))))
 
-(def ^:const topic "nba/scores")
-
-(defn start-consumer
-  [conn ^String username]
-  (mh/subscribe conn
-                [topic]
-                (fn [^String topic _ ^bytes payload]
-                  (println (format "[consumer] %s received %s" username (String. payload "UTF-8"))))))
-
-(defn -main
-  [& args]
-  (let [id    (mh/generate-id)
-        conn  (mh/connect "tcp://127.0.0.1:1883" id)
-        users ["joe" "aaron" "bob"]]
-    (doseq [u users]
-      (let [c (mh/connect "tcp://127.0.0.1:1883" (format "consumer.%s" u))]
-        (start-consumer c u)))
-    (mh/publish conn topic "BOS 101, NYK 89")
-    (mh/publish conn topic "ORL 85, ALT 88")
+(defn -main []
+  (let [id (mh/generate-id)
+        conn (mh/connect "tcp://127.0.0.1:1883" id)]
+    (over-pollution 50)
+    (dotimes [n 10] (mh/publish conn "Pollution" (str (* n 10))))
     (Thread/sleep 100)
-    (mh/disconnect conn)
-    ))
+    (mh/disconnect conn)))
+
+(-main)
